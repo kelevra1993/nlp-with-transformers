3f0b871ebc680e21dc7eedd3f7ca4e1ae237b08c
Robert Kelevra <kelevra.robert@gmail.com>
2024-06-27 22:38:22 +0200

    first commit

---
+++ chapter_2/chapter_2.py
@@ -0,0 +1,4 @@
+"""
+    This is the implementation of chapter 2 of the book natural language processing with Transformers.
+"""
+from utilities.utils import print_green, print_bold, print_blue, print_red, print_yellow
